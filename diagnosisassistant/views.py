a8e22855135c4cb5d4ade5e1041a39c87f5bc422
Yeferson Benavides <yabenavides@unicauca.edu.co>
2025-03-11 19:53:41 -0500

    fix: install missing app to invalidate refresh token on logout

--- diagnosisassistant/views.py
+++ diagnosisassistant/views.py
@@ -14,11 +14,10 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class LogoutView(APIView):
      permission_classes = (IsAuthenticated,)
      def post(self, request):
-          
           try:
                refresh_token = request.data["refresh_token"]
                token = RefreshToken(refresh_token)
                token.blacklist()
-               return Response(status=status.HTTP_205_RESET_CONTENT)
+               return Response('Refresh token invalidado con éxito', status=status.HTTP_200_OK)
           except Exception as e:
-               return Response(status=status.HTTP_400_BAD_REQUEST)
+               return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
