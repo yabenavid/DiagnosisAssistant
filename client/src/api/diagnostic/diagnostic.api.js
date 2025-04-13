8aa67e97177dbc67ea49ce3fbf95a508db8d117b
“Yeviles” <yaviles@unicauca.edu.co>
2025-04-13 11:03:02 -0500

    feat: update api diagnostic

--- client/src/api/diagnostic/diagnostic.api.js
+++ client/src/api/diagnostic/diagnostic.api.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
+import { getAuthHeaders } from '../../hooks/Authorization';
+
 
 const segmentApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/evaluate-images',
-    headers: {
-        "Content-Type": "multipart/form-data"
-    },
-    withCredentials: true
+    baseURL: 'http://127.0.0.1:8000/api/v1/evaluate-images',
+    ...getAuthHeaders()
 })
 
 
