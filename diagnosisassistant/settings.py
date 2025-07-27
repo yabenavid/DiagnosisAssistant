d5f81ebacd9b27b9b68aa9378e215493598fd20d
Yeferson Benavides <yabenavides@unicauca.edu.co>
2025-07-27 11:25:16 -0500

    feat: update allowed host address to new ipaddress and add cors allowed origin

--- diagnosisassistant/settings.py
+++ diagnosisassistant/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['18.189.186.29', 'localhost']
+ALLOWED_HOSTS = ['3.141.12.232', 'localhost']
 
 # Application definition
 
@@ -142,7 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors authorization
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
