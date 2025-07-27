a9fd56630819065266feed50d51b247e0057e3aa
“Yeviles” <yaviles@unicauca.edu.co>
2025-07-27 15:55:01 -0500

    feat: update images from about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { HomeCarousel, Footer, NavigationBar } from '../components';
+import Segmented from '../assets/segmented_1.png';
+import IngEmber from '../assets/IMG_8384.png';
+import IngRicardo from '../assets/IMG_8385.jpg';
+import IngYeferson from '../assets/IMG_8387.jpeg';
+import IngYennyfer from '../assets/IMG_8386.jpeg';
 
 import "/src/styles/About.css";
 
@@ -11,7 +16,7 @@ const AcercaDe = () => {
             <section className="about-section">
                 <div className="image-container">
                     <img 
-                        src="/src/assets/segmented_1.png" 
+                        src={Segmented}  
                         alt="Instrumental médico" 
                     />
                 </div>
@@ -30,14 +35,14 @@ const AcercaDe = () => {
                 <h2>Participantes</h2>
                 <div className="members-grid">
                     <div className="member-card">
-                        <img src="/src/assets/ember.png" alt="Ing. Ember Martinez" />
+                        <img src={IngEmber} alt="Ing. Ember Martinez" />
                         <h4>Ing. Ember Martinez (Director)</h4>
                         <p>
                             Ingeniero de Sistemas de la Universidad Nacional de Colombia, Sede Bogotá, graduado en 2001. Posteriormente, obtuvo una Maestría en Ingeniería con énfasis en Ingeniería de Sistemas y Computación de la Universidad del Valle en 2011. Actualmente es un Especialista en Redes y Servicios Telemáticos de la Universidad del Cauca.
                         </p>
                     </div>
                     <div className="member-card">
-                        <img src="/src/assets/ricardo.jpg" alt="Ing. Ricardo Zambrano" />
+                        <img src={IngRicardo} alt="Ing. Ricardo Zambrano" />
                         <h4>Ing. Ricardo Zambrano (Coodirector)</h4>
                         <p>
                             Ingeniero de Sistemas y Computación, Universidad de los Andes. Maestría en Administración de Negocios, Universidad de Phoenix. Diplomado en Gestión y Articulación de Procesos Asociativos, Universidad Javeriana. Diplomado en Gerencia de la Tecnología y la Innovación, Universidad del Cauca. Diplomado en Alta Gerencia, Universidad ICESI.
@@ -45,17 +50,20 @@ const AcercaDe = () => {
                     </div>
                     
                     <div className="member-card">
-                        <img src="/src/assets/yefer.jpeg" alt="Yeferson Benavides" />
-                        <h4>Yeferson Benavides (Estuadiante)</h4>
+                        <img src={IngYeferson} alt="Yeferson Benavides Marín" />
+                        <h4>Yeferson Benavides (Estudiante)</h4>
                         <p>
-                            Estudiante de Ingeniría de Sistema de la Universidad del Cauca.
+                            Estudiante de Ingeniería de Sistemas de la Universidad del Cauca.
+                            Actualmente colaborador en el área del Adobe Commerce (Magento) 
+
                         </p>
                     </div>
                     <div className="member-card">
-                        <img src="/src/assets/yenn.jpg" alt="Yennyfer Aviles" />
-                        <h4>Yennyfer Aviles (Estuadiante)</h4>
+                        <img src={IngYennyfer} alt="Yennyfer Aviles Ramírez" />
+                        <h4>Yennyfer Aviles (Estudiante)</h4>
                         <p>
-                            Estudiante de Ingeniría de Sistema de la Universidad del Cauca.
+                            Estudiante de Ingeniería de Sistemas de la Universidad del Cauca.
+                            Actualmente colaboradora en el área del Adobe Commerce (Magento)
                         </p>
                     </div>
                     
